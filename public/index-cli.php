233ad05410e0fbc8913a0d16e3db15955e851e99
Andrey Morozov <andreysssr@yandex.ru>
2023-05-10 23:23:25 +0300

    All code

--- public/index-cli.php
+++ public/index-cli.php
@@ -1,6 +1,5 @@
 <?php
 
-use app\service\WikiApi;
 use core\App\Cli\Application;
 use core\Container\Container;
 
@@ -17,38 +16,3 @@ $app = $container->get(Application::class);
 require 'config/listeners.php';
 
 $app->start();
-
-//=================================
-//$class = 'app\service\WikiApi';
-//$reflection = new \ReflectionClass($class);
-//$constructor = $reflection->getConstructor();
-
-//print_r($constructor);
-//print_r($constructor->getParameters());
-//
-
-//$arg = [];
-//foreach ($arr = $constructor->getParameters() as $parameter){
-//    echo $parameter->getName() . " - " . $parameter->getType() .PHP_EOL;
-//    $arg[] = (string) $parameter->getType();
-
-//    print_r(!$parameter->isDefaultValueAvailable());
-//    print_r($parameter->getName());
-//    print_r($parameter->getType());
-//    print_r($parameter->getAttributes());
-//}
-
-//print_r($arg);
-
-//=================================
-
-//$eventManager = $container->get('core\EventManager\EventManager');
-
-//print_r($eventManager);
-
-//print_r($container->get('config')['wiki']);
-
-//$wiki = new Wiki($container->get('config')['wiki']);
-
-//var_dump($container);
-
